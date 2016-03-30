368b347aef38701fcb1a6f43055638e0759cd0c3
Robert burner Schadek <rburners@gmail.com>
2016-03-30 17:52:30 +0200

    some more

--- source/app.d
+++ source/app.d
@@ -3,15 +3,40 @@ import pegged.grammar;
 
 mixin(grammar(`
 UML:
-	Sequence < Participent "->" Participent Node*
-	Participent <- (identifier / [(){}\[\]] / digits)+
-	Node < ":" NodeText+
-	NodeText <- identifier blank
+	Start < Comment / SequenceStart
+
+	SequenceStart < (Sequence / SequenceType)*
+	Sequence < Participent SequenceArrow Participent Note*
+	Participent <- (identifier / String)+
+	Note < ":" NoteText+
+	NoteText <- Text+
+	SequenceArrow < 
+		"o<-" / "->o" / "o<->" / "<->o"
+		/ "x<-" / "->x" / "<<-" / "->>"
+		/ "<-" / "<--" / "->" / "-->" 
+		/ "/-" / "\\-" / "-/" / "-\\"
+		/ "o//-" / "o\\\\-" / "-//o" / "-\\\\o"
+		/ "o/-" / "o\\-" / "-/o" / "-\\o"
+		/ "//-" / "\\\\-" / "-//" / "-\\\\"
+
+	SequenceType < 
+		"actor" Participent
+		/ "boundary" Participent
+		/ "control" Participent
+		/ "entity" Participent
+		/ "database" Participent
+
+	Comment <- "'" Text+
+	Text <- identifier / blank
+
+	String < '"' ([a-zA-Z0-9])* '"'
 `));
 
 void main()
 {
 	//writeln(UML("Alice -> Bob"));
-	writeln(UML("Alice -> Bob[10]"));
-	//writeln(UML("Alice -> Joe : Alice calls Joe\nmultiple times\n:and writes him letters"));
+	//writeln(UML("actor Foo\nFoo o//- Bar"));
+	writeln(UML("Alice -> \"Bob[10]\""));
+	//writeln(UML("' Some Comments"));
+	//writeln(UML("Alice ->o Joe : Alice calls Joe\nmultiple times\n:and writes him letters"));
 }
