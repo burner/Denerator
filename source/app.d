81a4c09ffaf17bb0b2b01408423a7e93ee6e5f46
Robert burner Schadek <rburners@gmail.com>
2016-03-30 15:49:21 +0200

    plantuml and pegged are just awesome together

--- source/app.d
+++ source/app.d
@@ -1,6 +1,17 @@
 import std.stdio;
+import pegged.grammar;
+
+mixin(grammar(`
+UML:
+	Sequence < Participent "->" Participent Node*
+	Participent <- (identifier / [(){}\[\]] / digits)+
+	Node < ":" NodeText+
+	NodeText <- identifier blank
+`));
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	//writeln(UML("Alice -> Bob"));
+	writeln(UML("Alice -> Bob[10]"));
+	//writeln(UML("Alice -> Joe : Alice calls Joe\nmultiple times\n:and writes him letters"));
 }
