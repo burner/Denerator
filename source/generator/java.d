f586d803b6e8503d195ec24d91ff3047e94c5e3d
Stephan Voskuhl <stephan.voskuhl@uni-oldenburg.de>
2017-11-28 17:39:44 +0100

    Added validation for members, finish member function generation for java degenerator

--- source/generator/java.d
+++ source/generator/java.d
@@ -6,14 +6,19 @@ import model.world;
 import model.classes;
 import model.entity;
 import model.component;
+import model.container;
 import std.exception : enforce;
 import std.typecons;
 import std.array : join, replace;
 import std.experimental.logger;
-import std.algorithm.mutation :reverse;
+import std.algorithm: map;
+import std.algorithm.mutation : reverse;
+import std.format;
+import containers.dynamicarray;
+import containers.hashmap;
 
 class Java : Generator {
-    import model.container;
+
     //The base path under which all files shall be placed
     immutable(string) outputDirBasePath;
     //Working path modified when generating output
@@ -36,7 +41,6 @@ class Java : Generator {
     override void generate(string technology){}
 
     void generate(in Container container){
-        info("Generating contents of container ", container.name);
         assert(container.technology is TECHNOLOGY_JAVA);
         this.currentContainer = container;
         immutable(string) outputDir = getOutputDir(container);
@@ -56,7 +60,6 @@ class Java : Generator {
     }
 
     void generateComponent(in Component component){
-        info("Generating contents of component ", component.name);
         immutable(string) outputDir = getOutputDir(component);
         enforce(Generator.createFolder(outputDir));
         //generate classes
@@ -73,24 +76,19 @@ class Java : Generator {
     }
 
     void generateClass(Parent)(in Class clazz, in Parent parent, in string outputDir) const{
-        info("Generating class " , clazz.name);
-
         //generating file to write to
         auto file = Generator.createFile([outputDir, clazz.name ~ ".java"]);
         auto lockingTextWriter = file.lockingTextWriter();
 
         //generating package line
-        format(lockingTextWriter, 0, getClassPackageLine(parent));
-        //TODO asssert that "Java" keys exist in protection and containerType attributes
+        generator.format(lockingTextWriter, 0, getClassPackageLine(parent));
 
         //generating class deklaration
-        format(lockingTextWriter, 0, "%s %s %s{ \n", clazz.protection[TECHNOLOGY_JAVA], clazz.containerType[TECHNOLOGY_JAVA], clazz.name );
+        generator.format(lockingTextWriter, 0, "%s %s %s{ \n", clazz.protection[TECHNOLOGY_JAVA], clazz.containerType[TECHNOLOGY_JAVA], clazz.name );
 
-        //generating members
-        //MemRange is an InputRange
         generateMembers(lockingTextWriter, clazz.members);
 
-        format(lockingTextWriter, 0, "\n}");
+        generator.format(lockingTextWriter, 0, "\n}");
     }
 
     void generateMembers(Out)(ref Out lockingTextWriter, in Member[] members) const{
@@ -114,25 +112,35 @@ class Java : Generator {
     }
 
     void generateMemberVariable(Out)(ref Out lockingTextWriter, in MemberVariable memberVariable) const{
-        //TODO assert that modifiers exist
-        string protection = memberVariable.protection[TECHNOLOGY_JAVA];
-        const(string) languageSpecificAttributes = memberVariable.langSpecificAttributes[TECHNOLOGY_JAVA].join(" ");
+        const(string) protection = getProtection(memberVariable);
+        const(string) languageSpecificAttributes = getLanguageSpecificAttributes(memberVariable);
         const(string) type = memberVariable.type.typeToLanguage[TECHNOLOGY_JAVA];
         const(string) name = memberVariable.name;
-
-        format(lockingTextWriter, 1, "%s %s %s %s;\n", protection, languageSpecificAttributes, type, name);
+        generator.format(lockingTextWriter, 1, "%s %s %s %s;\n", protection, languageSpecificAttributes, type, name);
     }
 
     void generateMemberFunction(Out)(ref Out lockingTextWriter, in MemberFunction memberFunction) const{
-        //TODO
+        validateMemberFunction(memberFunction);
+        const(string) protection = getProtection(memberFunction);
+        const(string) languageSpecificAttributes = getLanguageSpecificAttributes(memberFunction);
+        const(string) type = memberFunction.returnType.typeToLanguage[TECHNOLOGY_JAVA];
+        const(string) name = memberFunction.name;
+        const(string) parameterString = memberFunction.parameter[].map!(parameter => getMemberFunctionParameter(parameter)).join(", ");
+        generator.format(lockingTextWriter, 1, "%s %s %s %s (%s);", protection, languageSpecificAttributes, type, name, parameterString);
     }
 
-    void generateMemberFunctionParameter(Out)(ref Out lockingTextWriter, ref MemberVariable parameter){
-        //TODO
+    string getMemberFunctionParameter(in MemberVariable parameter) const{
+        validateMemberFunctionParameter(parameter);
+        const(string) name = parameter.name;
+        const(string) languageSpecificAttributes = getLanguageSpecificAttributes(parameter);
+        const(string) type = parameter.type.typeToLanguage[TECHNOLOGY_JAVA];
+        return std.format.format("%s %s %s", languageSpecificAttributes, type, name);
     }
 
-    string determineProtectionString(Out, PE : ProtectedEntity)(ref Out lockingTextWriter, ref PE protectedEntitiy){
-        //TODO
+    void validateMemberFunctionParameter(in MemberVariable memberVariable) const{
+        if(!(TECHNOLOGY_JAVA in memberVariable.type.typeToLanguage)){
+            throw new Exception(std.format.format("The type for parameter %s was not provided", memberVariable.name));
+        }
     }
 
     private immutable(string) getOutputDir(in Entity entity) const {
@@ -149,7 +157,6 @@ class Java : Generator {
         }
         folderNames ~= this.outputDirBasePath;
         folderNames.reverse();
-        info("Path: ",  folderNames.join("/"));
         return folderNames.join("/");
     }
 
@@ -174,5 +181,46 @@ class Java : Generator {
         string packageLine = "package " ~ packagePath;
         return packageLine;
     }
-}
 
+    private immutable(string) getLanguageSpecificAttributes(in Member member) const{
+        string languageSpecificAttributes;
+        if(TECHNOLOGY_JAVA in member.langSpecificAttributes){
+            languageSpecificAttributes = member.langSpecificAttributes[TECHNOLOGY_JAVA].join(" ");
+        }
+        return languageSpecificAttributes;
+    }
+
+    private immutable(string) getProtection(in Member member) const{
+        string protection;
+        if(TECHNOLOGY_JAVA in member.protection){
+            protection = member.protection[TECHNOLOGY_JAVA];
+        }
+        return protection;
+    }
+
+/* validation *********************************************************************************************************/
+
+    void validateMemberFunction(in MemberFunction memberFunction) const{
+        if(memberFunction.name is null){
+            throw new Exception("The name of a member function must be defined.");
+        }
+        if(memberFunction.returnType is null){
+            throw new Exception(std.format.format("A type must be defined for %s but type was null"), memberFunction.name);
+        }
+        if(!(TECHNOLOGY_JAVA in memberFunction.returnType.typeToLanguage)){
+            throw new Exception(std.format.format("The type %s cannot be translated to java because typeToLanguage has not been provided.", memberFunction.returnType.name));
+        }
+    }
+
+    void validateMemberVariable(in MemberVariable memberVariable) const{
+        if(!(TECHNOLOGY_JAVA in memberVariable.type.typeToLanguage)){
+            throw new Exception(std.format.format("The type %s cannot be translated to java because typeToLanguage has not been provided.", memberVariable.type.name));
+        }
+        if(memberVariable.type is null){
+            throw new Exception(std.format.format("A type must be defined for %s but type was null"), memberVariable.name);
+        }
+        if(!(TECHNOLOGY_JAVA in memberVariable.type.typeToLanguage)){
+            throw new Exception(std.format.format("The type %s cannot be translated to java because typeToLanguage has not been provided.", memberVariable.type.name));
+        }
+    }
+}
