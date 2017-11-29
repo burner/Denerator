d9ad15791bda1c3ad68d744bd81a9cce667e46d0
Stephan Voskuhl <stephan.voskuhl@uni-oldenburg.de>
2017-11-29 13:19:05 +0100

    Add support for importing classes

--- source/generator/java.d
+++ source/generator/java.d
@@ -18,7 +18,13 @@ import containers.dynamicarray;
 import containers.hashmap;
 
 class Java : Generator {
-
+    //For these types no further imports are required
+    string[] primitiveTypes = ["void", "Void", "byte", "Byte", "short", "Short", "int", "Integer", "long", "Long",
+                "float", "Float", "double", "Double", "String", "boolean", "Boolean", "char", "Character"];
+    //e.g. [class_name] = [package1.class_name, packag2.class_name]
+    string[][string] newTypeMap;
+    //e.g. [path_to_class_file] = [requested_class_name1, ...]
+    string[][string] requestedTypeMap;
     //The base path under which all files shall be placed
     immutable(string) outputDirBasePath;
     //Working path modified when generating output
@@ -57,6 +63,43 @@ class Java : Generator {
             const(Component) component = value;
             generateComponent(component);
         }
+
+        foreach(path, requestedTypes; this.requestedTypeMap){
+            if(!(requestedTypes.length == 0)){
+                //copy file content because the package declaration needs to be prepended
+                File readFile = File(path, "r");
+                string packageLine = readFile.readln();
+                string fileContent;
+                while (!readFile.eof()) {
+                    fileContent ~= readFile.readln();
+                }
+                readFile.close();
+                File writeFile = File(path, "w");
+                writeFile.write(packageLine);
+                writeFile.write(getImportLines(requestedTypes));
+                writeFile.write(fileContent);
+                writeFile.close();
+
+            }
+        }
+    }
+
+    string getImportLines(in string[] requestedTypes){
+        string importLines;
+        foreach(requestedType; requestedTypes){
+            if(requestedType in this.newTypeMap){
+                string[] packages = this.newTypeMap[requestedType];
+                if(packages.length > 1){
+                    error(std.format.format("More than one declaration of class %s can be found. The import has to be executed manually.", requestedType));
+                    break;
+                }
+                importLines ~= "import " ~ packages[0] ~ ";\n";
+            }
+            else{
+                error(std.format.format("Requested type %s could not be found.", requestedType));
+            }
+        }
+        return importLines;
     }
 
     void generateComponent(in Component component){
@@ -75,26 +118,39 @@ class Java : Generator {
         }
     }
 
-    void generateClass(Parent)(in Class clazz, in Parent parent, in string outputDir) const{
+    void generateClass(Parent)(in Class clazz, in Parent parent, in string outputDir) {
+        string[] requestedTypes;
         //generating file to write to
-        auto file = Generator.createFile([outputDir, clazz.name ~ ".java"]);
+        immutable(string) path = [outputDir, clazz.name ~ ".java"].join("/");
+        auto file = Generator.createFile(path);
         auto lockingTextWriter = file.lockingTextWriter();
 
         //generating package line
+        const(string) classPackageLine = getClassPackageLine(parent);
         generator.format(lockingTextWriter, 0, getClassPackageLine(parent));
 
-        //generating class deklaration
+        //for imports
+        if(clazz.name in this.newTypeMap){
+            this.newTypeMap[clazz.name] ~= [getPackagePath(parent) ~ "." ~ clazz.name];
+        } else{
+            this.newTypeMap[clazz.name] = [getPackagePath(parent) ~ "." ~ clazz.name];
+        }
+
+        //mark this as a new type for imports
         immutable string line = [clazz.protection[TECHNOLOGY_JAVA], clazz.containerType[TECHNOLOGY_JAVA], clazz.name]
                 .filter!(str => str.length > 0)
                 .join(" ");
         generator.format(lockingTextWriter, 0, "%s{ \n", line);
 
-        generateMembers(lockingTextWriter, clazz.members);
+        generateMembers(lockingTextWriter, clazz.members, requestedTypes);
 
         generator.format(lockingTextWriter, 0, "\n}");
+
+        //mark types needed for imports
+        this.requestedTypeMap[path] = requestedTypes;
     }
 
-    void generateMembers(Out)(ref Out lockingTextWriter, in Member[] members) const{
+    void generateMembers(Out)(Out lockingTextWriter, in Member[] members, ref string[] requestedTypes) {
         const(MemberVariable)[] memberVariables;
         const(MemberFunction)[] memberFunctions;
         //sort members
@@ -105,48 +161,61 @@ class Java : Generator {
                 memberFunctions ~= memberFunction;
             }
         }
-        //generate member variables
         foreach(memberVariable; memberVariables){
-            generateMemberVariable(lockingTextWriter, memberVariable);
+            generateMemberVariable(lockingTextWriter, memberVariable, requestedTypes);
         }
         foreach(memberFunction; memberFunctions){
-            generateMemberFunction(lockingTextWriter, memberFunction);
+            generateMemberFunction(lockingTextWriter, memberFunction, requestedTypes);
         }
     }
 
-    void generateMemberVariable(Out)(ref Out lockingTextWriter, in MemberVariable memberVariable) const{
+    void generateMemberVariable(Out)(Out lockingTextWriter, in MemberVariable memberVariable, ref string[] requestedTypes) {
         const(string) protection = getProtection(memberVariable);
         const(string) languageSpecificAttributes = getLanguageSpecificAttributes(memberVariable);
-        const(string) type = memberVariable.type.typeToLanguage[TECHNOLOGY_JAVA];
+        const(string) type = getTypeString(memberVariable.type, requestedTypes);
         const(string) name = memberVariable.name;
         string[] container = [protection, languageSpecificAttributes, type, name];
         generator.format(lockingTextWriter, 1, "%s;\n", container.join(" "));
     }
 
-    void generateMemberFunction(Out)(ref Out lockingTextWriter, in MemberFunction memberFunction) const{
+
+    void generateMemberFunction(Out)(Out lockingTextWriter, in MemberFunction memberFunction, ref string[] requestedTypes){
         validateMemberFunction(memberFunction);
         const(string) protection = getProtection(memberFunction);
         const(string) languageSpecificAttributes = getLanguageSpecificAttributes(memberFunction);
-        const(string) type = memberFunction.returnType.typeToLanguage[TECHNOLOGY_JAVA];
+        const(string) type = getTypeString(memberFunction.returnType, requestedTypes);
         const(string) name = memberFunction.name;
         const(string) functionString = [protection, languageSpecificAttributes, type, name].filter!(str => str.length > 0).join(" ");
-        const(string) parameterString = memberFunction.parameter[].map!(parameter => getMemberFunctionParameter(parameter)).join(", ");
+        const(string) parameterString = memberFunction.parameter[].map!(parameter => getMemberFunctionParameter(parameter, requestedTypes)).join(", ");
         generator.format(lockingTextWriter, 1, "%s(%s);", functionString, parameterString);
     }
 
-    string getMemberFunctionParameter(in MemberVariable parameter) const{
+    string getMemberFunctionParameter(in MemberVariable parameter, ref string[] requestedTypes){
         validateMemberFunctionParameter(parameter);
         const(string) name = parameter.name;
         const(string) languageSpecificAttributes = getLanguageSpecificAttributes(parameter);
-        const(string) type = parameter.type.typeToLanguage[TECHNOLOGY_JAVA];
+        const(string) type = getTypeString(parameter.type, requestedTypes);
         return [languageSpecificAttributes, type, name].filter!(str => str.length > 0).join(" ");
     }
 
-    void validateMemberFunctionParameter(in MemberVariable memberVariable) const{
-        if(!(TECHNOLOGY_JAVA in memberVariable.type.typeToLanguage)){
-            throw new Exception(std.format.format("The type for parameter %s was not provided", memberVariable.name));
+    string getTypeString(in Type type, ref string[] requestedTypes) {
+            string typeJavaName = type.typeToLanguage[TECHNOLOGY_JAVA];
+            if(!isPrimitive(type)){
+                requestedTypes ~= type.name;
+            }
+            return typeJavaName;
         }
+
+    bool isPrimitive(in Type type){
+        bool isPrimitiveType = false;
+            foreach(primitiveType ; primitiveTypes){
+                if(type.typeToLanguage[TECHNOLOGY_JAVA] == primitiveType){
+                    isPrimitiveType = true;
+                }
+            }
+        return isPrimitiveType;
     }
+
 
     private immutable(string) getOutputDir(in Entity entity) const {
         string[] folderNames = [];
@@ -170,7 +239,14 @@ class Java : Generator {
     }
 
     private immutable(string) getClassPackageLine(in Component parent) const {
-        Rebindable!(const(Component)) parentRebindable = parent;
+        string packagePath = getPackagePath(parent);
+        packagePath ~= ";\n";
+        string packageLine = "package " ~ packagePath;
+        return packageLine;
+    }
+
+    private immutable(string) getPackagePath(C)(in C parent) const{
+        Rebindable!(const(Component)) parentRebindable = cast(Component)parent;
         string[] packagePathReverse = [];
         while(true){
             packagePathReverse ~= parentRebindable.name;
@@ -181,10 +257,7 @@ class Java : Generator {
             }
         }
         packagePathReverse.reverse;
-        string packagePath = packagePathReverse.join(".");
-        packagePath ~= ";\n";
-        string packageLine = "package " ~ packagePath;
-        return packageLine;
+        return packagePathReverse.join(".");
     }
 
     private immutable(string) getLanguageSpecificAttributes(in Member member) const{
@@ -228,4 +301,10 @@ class Java : Generator {
             throw new Exception(std.format.format("The type %s cannot be translated to java because typeToLanguage has not been provided.", memberVariable.type.name));
         }
     }
+
+    void validateMemberFunctionParameter(in MemberVariable memberVariable) const{
+            if(!(TECHNOLOGY_JAVA in memberVariable.type.typeToLanguage)){
+                throw new Exception(std.format.format("The type for parameter %s was not provided", memberVariable.name));
+            }
+        }
 }
