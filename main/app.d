36445ae2937615623d99c9b4e77776b2b5ea914c
Stephan Voskuhl <stephan.voskuhl@uni-oldenburg.de>
2018-04-03 11:02:53 +0200

    Refactor app.d such that functions for entity modifications are now within entity_modification.d Generate world in main function, not within architecture building function because multiple systems must be supported

--- main/app.d
+++ main/app.d
@@ -16,8 +16,9 @@ import predefined.types.group;
 import predefined.types.address;
 import predefined.types.basictypes;
 import predefined.ctrl.userctrl;
-
 import predefined.angular.component;
+
+import entity_modification;
 
 class NoTimeLogger : Logger {
 	import std.stdio : writefln;
@@ -44,11 +45,19 @@ void main() {
 		ret = true;
 	}
 	logf("%s", ret);
-	if(!ret) appModel();
+	if(!ret){
+	    //Basic setup
+        auto world = new TheWorld("World");
+        addBasicTypes(world);
+	    appModel(world);
+	}
 }
 
-void appModel(){
+void appModel(ref TheWorld world){
     import generator.java;
+
+    //Add java specific types:
+    addExternalTypes(world);
 
     //Classes container for peroforming operations on all classes that need to be generated
     Class[] generatedClasses;
@@ -64,12 +73,6 @@ void appModel(){
     MemberVariable[] protectedMemberVariables;
 
     MemberVariable[] beanMemberVariable;
-
-    //Basic setup
-    auto world = new TheWorld("World");
-    addBasicTypes(world);
-    addExternalTypes(world);
-    Type voidType = world.getType("Void");
 
     //System context level
     Actor runner = world.newActor("Runner");
@@ -3963,96 +3966,3 @@ void addStartingDisplayPresenterMembers(ref TheWorld world, ref MemberVariable[]
     protectedMemberVariables ~= startingDisplayContractView;
 }
 
-
-
-T[] modifyElements(T : Entity, F : T function(T))(T[] entitiesToModify, F[] functions){
-    T[] result;
-    bool first = true;
-    foreach(function_ ; functions){
-        if(first){
-            foreach(entity ; entitiesToModify){
-                result ~= function_(entity);
-            }
-        }
-        else{
-            for( int i = 0; i < result.length; ++i ){
-                function_(result[i]);
-            }
-        }
-        first = false;
-    }
-    return result;
-}
-
-T setDoNotGenerateFlagToYes(T : Class)(T clazz){
-    clazz.doNotGenerate = DoNotGenerate.yes;
-    return clazz;
-}
-
-T setDoNotGenerateFlagToNo(T)(T entity){
-    entity.doNotGenerate = DoNotGenerate.no;
-    return entity;
-}
-
-T setContainerTypeToJavaClass(T)(T entity){
-    entity.containerType["Java"] = "class";
-    return entity;
-}
-
-T setContainerTypeToJavaEnum(T : Class)(T clazz){
-    clazz.containerType["Java"] = "enum";
-    return clazz;
-}
-
-T setContainerTypeToJavaInterface(T : Class)(T clazz){
-   clazz.containerType["Java"] = "interface";
-   return clazz;
-}
-
-T setProtectionToJavaPublic(T : ProtectedEntity)(T entity){
-    entity.protection["Java"] = "public";
-    return entity;
-}
-
-T setProtectionToJavaProtected(T : ProtectedEntity)(T entity){
-    entity.protection["Java"] = "protected";
-    return entity;
-}
-
-T setModelTypeNameAsJavaClassName(T : Type)(T type){
-    type.typeToLanguage["Java"] = type.name;
-    return type;
-}
-
-T addModifierAbstract(T: Class)(T clazz){
-    clazz.languageSpecificAttributes["Java"] ~= "abstract";
-    return clazz;
-}
-
-T addModifierAbstract(T : Member)(T member){
-    member.langSpecificAttributes["Java"] ~= "abstract";
-    return member;
-}
-
-MemberFunction addSetter(ref MemberVariable memberVariable, ref MemberFunction[] memberFunctionsContainer, Class clazz, TheWorld world){
-    MemberFunction setter = clazz.newMemberFunction(getSetOrGetFunctionName(memberVariable, "set"));
-    setter.returnType = world.getType("Void");
-    setter.addParameter(memberVariable.name, memberVariable.type);
-    memberFunctionsContainer ~= setter;
-    return setter;
-}
-
-MemberFunction addGetter(ref MemberVariable memberVariable, ref MemberFunction[] memberFunctionsContainer, Class clazz, TheWorld world){
-    MemberFunction getter = clazz.newMemberFunction(getSetOrGetFunctionName(memberVariable, "get"));
-    getter.returnType = memberVariable.type;
-    memberFunctionsContainer ~= getter;
-    return getter;
-}
-
-string getSetOrGetFunctionName(ref MemberVariable memberVariable, string setOrGet){
-    import std.ascii : toUpper;
-    char[] functionNamePart = memberVariable.name.dup;
-    assert(functionNamePart.length > 0, "The name of a function must consist of at least 1 character.");
-    functionNamePart[0] = toUpper(functionNamePart[0]);
-    return (setOrGet ~ functionNamePart).idup;
-}
